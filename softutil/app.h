ad823d6fc88213548a435a742001f6a9745c7382
drclcomputers <132164125+drclcomputers@users.noreply.github.com>
2024-06-15 17:55:49 +0300

    main

--- softutil/app.h
+++ softutil/app.h
@@ -19,6 +19,133 @@ class app {
 public:
 	natural buget, venit;
 
+	void income() {
+		cout << "------------------Income------------------\n\n";
+		cout << "Enter income: ";
+		natural suma; cin >> suma;
+		while (suma < 0) {
+			cout << "Enter correct income: ";
+			cin >> suma;
+		}
+		venit += suma;
+		cout << "Types:\n1. Scholarship\n2. Paycheck\n3. Sold stuff\n4. Others\n\nEnter income type: ";
+		natural type; cin >> type;
+		while (type < 0 || type>4) {
+			cout << "Enter correct type of income: ";
+			cin >> type;
+		}
+		ofstream fin("income.pg", ios::app);
+		fin << suma;
+		fin << setw(5) << " ";
+		if (type == 1) fin << "Scholarship";
+		else if (type == 2) fin << "Paycheck";
+		else if (type == 3) fin << "Sold-Stuff";
+		else if (type == 4) {
+			cout << "Custom income: ";
+			char customtype[100]; cin.get(); cin.getline(customtype, 100);
+			fin << customtype;
+		}
+
+		fin << setw(5) << " ";
+		time_t rawtime = time(0);
+		char* date_time = ctime(&rawtime);
+		fin << date_time << '\n';
+
+
+		fin.close();
+		ofstream fin1("venit.pg", ios::trunc);
+		fin1 << venit;
+		fin1.close();
+	}
+
+	void expense() {
+		cout << "------------------Expenses------------------\n\n";
+		cout << "Enter expense: ";
+		natural suma; cin >> suma;
+		while (suma < 0) {
+			cout << "Enter correct expense: ";
+			cin >> suma;
+		}
+		venit -= suma;
+		cout << "Types:\n1. Food\n2. Clothes\n3. Books\n4. Devices\n5. School Supplies\n6. Home Supplies\n7. Others\n\nEnter income type: ";
+		natural type; cin >> type;
+		while (type < 0 || type>7) {
+			cout << "Enter correct type of expense: ";
+			cin >> type;
+		}
+		ofstream fin("expense.pg", ios::app);
+		fin << suma;
+		fin << setw(5) << " ";
+		if (type == 1) fin << "Food";
+		else if (type == 2) fin << "Clothes";
+		else if (type == 3) fin << "Books";
+		else if (type == 4) fin << "Devices";
+		else if (type == 5) fin << "School-Supplies";
+		else if (type == 6) fin << "Home-Supplies";
+		else if (type == 7) {
+			cout << "Custom expense: ";
+			char customtype[101]; cin.get(); cin.getline(customtype, 100);
+			fin << customtype;
+		}
+
+		fin << setw(5) << " ";
+		time_t rawtime = time(0);
+		char* date_time = ctime(&rawtime);
+		fin << date_time << '\n';
+
+
+		fin.close();
+		ofstream fin1("venit.pg", ios::trunc);
+		fin1 << venit;
+		fin1.close();
+	}
+
+	void setbudget() {
+		cout << "------------------Set-Budget------------------\n\n";
+		cout << "Enter budget: "; cin >> buget;
+		while (buget < 0) {
+			cout << "Enter correct budget: ";
+			cin >> buget;
+		}
+		ofstream fin("buget.pg", ios::trunc);
+		fin << buget;
+		fin.close();
+	}
+
+
+	void showincome() {
+		cout << right;
+		cout << setw(6) << "Sum"
+			<< setw(5) << " " << "Description"
+			<< setw(10) << " " << "Date";
+		cout << "\n\n";
+		ifstream fin("income.pg", ios::app);
+		for (int i = 1; fin; i++) {
+			char text[200]; fin.getline(text, 200);
+			cout << i << ". " << text << '\n';
+			fin.get();
+		}
+		cout << '\n';
+		fin.close();
+	}
+
+	void showexpense() {
+		cout << right;
+		cout << setw(6) << "Sum"
+			<< setw(5) << " " << "Description"
+			<< setw(10) << " " << "Date";
+		cout << "\n\n";
+		ifstream fin("expense.pg", ios::app);
+		for (int i = 1; fin; i++) {
+			char text[200]; fin.getline(text, 200);
+			cout << i << ". " << text << '\n';
+			fin.get();
+		}
+		cout << '\n';
+		fin.close();
+	}
+
+
 	void savings_deposit() {
 		cout << "\nEnter sum to deposit: ";
 		natural suma, tot; cin >> suma;
@@ -99,6 +226,15 @@ public:
 	}
 
 
+	void resetsavings() {
+		cout << "Are you sure to delete all your savings: ";
+		char ans[5]; cin >> ans;
+		if (strstr(ans, "yes")) {
+			ofstream fin1("savings.pg", ios::trunc);
+			fin1.close();
+		}
+	}
+
 	void resetincome() {
 		cout << "Are you sure to delete all your incomes: ";
 		char ans[5]; cin >> ans;
@@ -131,134 +267,13 @@ public:
 			ofstream fin5("venit.pg", ios::trunc);
 			fin5 << 0;
 			fin5.close();
+			ofstream fin6("savings.pg", ios::trunc);
+			fin6 << 0;
+			fin6.close();
 		}
 	}
 
-	void showincome() {
-		cout << right;
-		cout << setw(6) <<"Sum"
-			 << setw(5) <<" " << "Description"
-			 << setw(10) <<" " << "Date";
-		cout << "\n\n";
-		ifstream fin("income.pg", ios::app);
-		for (int i = 1; fin; i++) {
-			char text[200]; fin.getline(text, 200);
-			cout << i << ". " << text << '\n';
-			fin.get();
-		}
-		cout << '\n';
-		fin.close();
-	}
 
-	void showexpense() {
-		cout << right;
-		cout << setw(6) << "Sum"
-			<< setw(5) << " " << "Description"
-			<< setw(10) << " " << "Date";
-		cout << "\n\n";
-		ifstream fin("expense.pg", ios::app);
-		for (int i = 1; fin; i++) {
-			char text[200]; fin.getline(text, 200);
-			cout << i << ". " << text << '\n';
-			fin.get();
-		}
-		cout << '\n';
-		fin.close();
-	}
-
-	void setbudget() {
-		cout << "------------------Set-Budget------------------\n\n";
-		cout << "Enter budget: "; cin >> buget;
-		while (buget < 0) {
-			cout << "Enter correct budget: ";
-			cin >> buget;
-		}
-		ofstream fin("buget.pg", ios::trunc);
-		fin << buget;
-		fin.close();
-	}
-
-	void income() {
-		cout << "------------------Income------------------\n\n";
-		cout << "Enter income: ";
-		natural suma; cin >> suma;
-		while (suma < 0) {
-			cout << "Enter correct income: ";
-			cin >> suma;
-		}
-		venit += suma;
-		cout << "Types:\n1. Scholarship\n2. Paycheck\n3. Sold stuff\n4. Others\n\nEnter income type: ";
-		natural type; cin >> type;
-		while (type < 0 || type>4) {
-			cout << "Enter correct type of income: ";
-			cin >> type;
-		}
-		ofstream fin("income.pg", ios::app);
-		fin << suma;
-		fin << setw(5) <<" ";
-		if (type == 1) fin << "Scholarship";
-		else if (type == 2) fin << "Paycheck";
-		else if (type == 3) fin << "Sold-Stuff";
-		else if (type == 4) {
-			cout << "Custom income: ";
-			char customtype[100]; cin.get(); cin.getline(customtype, 100);
-			fin << customtype ;
-		}
-
-		fin << setw(5) << " ";
-		time_t rawtime=time(0);
-		char* date_time = ctime(&rawtime);
-		fin << date_time << '\n';
-
-
-		fin.close();
-		ofstream fin1("venit.pg", ios::trunc);
-		fin1 << venit;
-		fin1.close();
-	}
-
-
-	void expense() {
-		cout << "------------------Expenses------------------\n\n";
-		cout << "Enter expense: ";
-		natural suma; cin >> suma;
-		while (suma < 0) {
-			cout << "Enter correct expense: ";
-			cin >> suma;
-		}
-		venit -= suma;
-		cout << "Types:\n1. Food\n2. Clothes\n3. Books\n4. Devices\n5. School Supplies\n6. Home Supplies\n7. Others\n\nEnter income type: ";
-		natural type; cin >> type;
-		while (type < 0 || type>7) {
-			cout << "Enter correct type of expense: ";
-			cin >> type;
-		}
-		ofstream fin("expense.pg", ios::app);
-		fin << suma;
-		fin << setw(5) << " ";
-		if (type == 1) fin << "Food";
-		else if (type == 2) fin << "Clothes";
-		else if (type == 3) fin << "Books";
-		else if (type == 4) fin << "Devices";
-		else if (type == 5) fin << "School-Supplies";
-		else if (type == 6) fin << "Home-Supplies";
-		else if (type == 7) {
-			cout << "Custom expense: ";
-			char customtype[101]; cin.get(); cin.getline(customtype, 100);
-			fin << customtype;
-		}
-
-		fin << setw(5) << " ";
-		time_t rawtime = time(0);
-		char* date_time = ctime(&rawtime);
-		fin << date_time << '\n';
-
-
-		fin.close();
-		ofstream fin1("venit.pg", ios::trunc);
-		fin1 << venit;
-		fin1.close();
-	}
 
 	void start() {
 		ifstream fin("venit.pg");
@@ -305,6 +320,9 @@ public:
 		else if (strstr(com, "resetexpense")) {
 			resetexpense();
 		}
+		else if (strstr(com, "resetsavings")) {
+			resetsavings();
+		}
 		else if (strstr(com, "reset")) {
 			reset();
 		}
@@ -314,6 +332,4 @@ public:
 		system(CLEAR_SCREEN);
 		start();
 	}
-
-
 };
